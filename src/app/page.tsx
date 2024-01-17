cc0feb0c9ba16b22ad8417b6a70d26139e89e7df
두동현 <doomole@atoncorp.com>
2024-01-17 14:48:21 +0900

    fix: 수정 #4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,13 @@
 
 import useLink from '@/hooks/useLink'
 import { useEffect } from 'react'
-import { useRecoilValue } from 'recoil'
-import { isLoadingAtom } from '@/recoil/atoms'
 
 export default function Home() {
-  const isLoading = useRecoilValue(isLoadingAtom)
   const { onLink } = useLink()
 
   useEffect(() => {
-    if (isLoading) {
+    setTimeout(function () {
       onLink('/home')
-    }
+    }, 2000)
   }, [])
 }
